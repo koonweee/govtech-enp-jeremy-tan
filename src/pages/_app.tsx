9e56c167df2f600930f861ffdb21254e39fd817a
Jeremy Tan <jeremytkw98@gmail.com>
2022-09-20 23:31:08 -0700

    check for vercel url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ const App: AppType = ({ Component, pageProps }) => {
 
 export default withTRPC<ServerRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
       ? `https://govtech-enp-jeremy-tan.vercel.app/api/trpc`
       : "http://localhost:3000/api/trpc";
 
