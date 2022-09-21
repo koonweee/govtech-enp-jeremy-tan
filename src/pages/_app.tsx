4f43b229c826c9d5c4d185c8d46ba3c6246fe639
Jeremy Tan <jeremytkw98@gmail.com>
2022-09-21 14:18:15 -0700

    fix server side css injection issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { withTRPC } from "@trpc/next";
 import { AppType } from "next/dist/shared/lib/utils";
 import type { ServerRouter } from "@/server/router";
+import React from "react";
 
-const App: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const App: AppType = function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if(jssStyles){
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+  return <Component {...pageProps} />
+}
 
 export default withTRPC<ServerRouter>({
   config({ ctx }) {
